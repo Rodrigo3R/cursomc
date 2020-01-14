be90f073f9669374c46997ae20108b6a9380530d
Rodrigo Rocha Rodrigues <triplorforex@gmail.com>
2020-01-14 08:39:40 -0400

    testando a primeira classe de dominio

--- src/main/java/com/gateway3r/cursomc/resources/CategoriaRecource.java
+++ src/main/java/com/gateway3r/cursomc/resources/CategoriaRecource.java
@@ -1,16 +1,29 @@
 package com.gateway3r.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gateway3r.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaRecource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"informática");
+		Categoria cat2 = new Categoria(2,"escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
